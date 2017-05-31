d965f52ba8497786d8f52d2695f86ebe0b2e8b1a
Yujiro Saito <yujiro@Yujiro-Saito-no-MacBook-Air.local>
2017-05-31 23:02:59 +0900

    UIDesign: DetailVC.collectionView

---
+++ Project6/DetailCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  DetailCollectionViewCell.swift
+//  Project6
+//
+//  Created by  Yujiro Saito on 2017/05/31.
+//  Copyright © 2017年 yujiro_saito. All rights reserved.
+//
+
+import UIKit
+
+class DetailCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var detailItemImage: UIImageView!
+    
+    
+    
+    
+}
--- Project6/DetailViewController.swift
+++ Project6/DetailViewController.swift
@@ -7,12 +7,12 @@
 //
 
 import UIKit
-import ImageSlideshow
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     //Outlet
-    @IBOutlet weak var slideshow: ImageSlideshow!
+    
+    @IBOutlet weak var detailCollectionView: UICollectionView!
     @IBOutlet weak var detailImage: UIImageView!
     @IBOutlet weak var detailName: UILabel!
     @IBOutlet weak var detailStarNum: UILabel!
@@ -29,56 +29,53 @@ class DetailViewController: UIViewController {
     
     
     
-    let localSource = [ImageSource(imageString: "img1")!, ImageSource(imageString: "img2")!, ImageSource(imageString: "img3")!, ImageSource(imageString: "img4")!]
-    
-    let alamofireSource = [AlamofireSource(urlString: "https://images.unsplash.com/photo-1432679963831-2dab49187847?w=1080")!, AlamofireSource(urlString: "https://images.unsplash.com/photo-1447746249824-4be4e1b76d66?w=1080")!, AlamofireSource(urlString: "https://images.unsplash.com/photo-1463595373836-6e0b0a8ee322?w=1080")!]
-    
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        /*
-
-        let url = URL(string: imageURL!)
-
-        DetailViewController.imageCache.object(forKey: "DetailNewImage")
-        
-        DispatchQueue.main.async {
-            do {
-                
-                let imgData: Data = try  NSData(contentsOf:url!,options: NSData.ReadingOptions.mappedIfSafe) as Data
-                
-                let img = UIImage(data: imgData)
-                self.detailImage.image = img
-                DetailViewController.imageCache.setObject(img!, forKey: "DetailNewImage")
-                
-            } catch {
-                
-                print(error.localizedDescription)
-            }
-        
-        }
+        detailCollectionView.delegate = self
+        detailCollectionView.dataSource = self
         
         
         
         
-        detailName.text = name
-        detailStarNum.text = starNum
-        detailCategoryName.text = categoryName
-        detailWhatContent.text = whatContent
         
-        self.slideshow.setImageInputs(alamofireSource)
-        self.slideshow.contentScaleMode = .scaleAspectFill
-        self.slideshow.slideshowInterval = 5
-        self.slideshow.zoomEnabled = true
-        self.slideshow.pageControlPosition = .hidden
-        self.slideshow.activityIndicator = DefaultActivityIndicator(style: .whiteLarge, color: .black)
         
-        let recognizer = UITapGestureRecognizer(target: self, action: #selector(DetailViewController.didTap))
-        slideshow.addGestureRecognizer(recognizer)
-*/
+        
+        
     }
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let detailImage = detailCollectionView.dequeueReusableCell(withReuseIdentifier: "DetailItems", for: indexPath) as? DetailCollectionViewCell
+        
+        detailImage?.detailItemImage.image = UIImage(named: "wantedly")
+        
+        
+        
+        
+        
+        return detailImage!
+        
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    
+    
+    
+    
     
     
     
@@ -107,33 +104,17 @@ class DetailViewController: UIViewController {
         
         
         
-        
         detailName.text = name
         detailStarNum.text = starNum
         detailCategoryName.text = categoryName
         detailWhatContent.text = whatContent
         
-        self.slideshow.setImageInputs(alamofireSource)
-        self.slideshow.contentScaleMode = .scaleAspectFill
-        self.slideshow.slideshowInterval = 5
-        self.slideshow.zoomEnabled = true
-        self.slideshow.pageControlPosition = .hidden
-        self.slideshow.activityIndicator = DefaultActivityIndicator(style: .whiteLarge, color: .black)
         
-        let recognizer = UITapGestureRecognizer(target: self, action: #selector(DetailViewController.didTap))
-        slideshow.addGestureRecognizer(recognizer)
         
         
         
     }
     
-    func didTap() {
-        let fullScreenController = slideshow.presentFullScreenController(from: self)
-        // set the activity indicator for full screen controller; skip the line if no activity indicator should be shown
-        fullScreenController.slideshow.activityIndicator = DefaultActivityIndicator(style: .white, color: nil)
-    }
-
-
    
 
 }
