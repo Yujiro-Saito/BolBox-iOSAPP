98ac373ce765d1bc754feb5d2d15c1d96e94ac16
Yujiro Saito <yujiro@Yujiro-Saito-no-MacBook-Air.local>
2017-08-21 22:05:21 +0900

    日本語エンコード

--- Project6/InDetailViewController.swift
+++ Project6/InDetailViewController.swift
@@ -133,15 +133,88 @@ class InDetailViewController: UIViewController {
     @IBAction func goSeeButtonDidTap(_ sender: Any) {
         
         
-        //safari開く
+        let targetURL = self.linkURL
+        let encodedURL = targetURL?.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)
         
-        let safariVC = SFSafariViewController(url: URL(string: self.linkURL!)!)
-        self.present(safariVC, animated: true, completion: nil)
+        //URL正式
+        guard let finalUrl = URL(string: encodedURL!) else {
+            print("無効なURL")
+            return
+        }
         
         
-        //performSegue(withIdentifier: "webview", sender: nil)
+        if (encodedURL?.contains("https"))! || (encodedURL?.contains("http"))! {
+            
+            //httpかhttpsで始まってるか確認
+            if (encodedURL?.hasPrefix("https"))! || (encodedURL?.hasPrefix("http"))! {
+                //http(s)で始まってる場合safari起動
+                let safariVC = SFSafariViewController(url: finalUrl)
+                self.present(safariVC, animated: true, completion: nil)
+                
+            }
+                //Httpsの場合
+                else if let range = encodedURL?.range(of: "https") {
+                    let startPosition = encodedURL?.characters.distance(from: (encodedURL?.characters.startIndex)!, to: range.lowerBound)
+                
+                    //4番目から最後までをURLとして扱う
+                    
+                    let indexNumber = startPosition
+                    
+                    let validURLString = (encodedURL?.substring(with: (encodedURL?.index((encodedURL?.startIndex)!, offsetBy: indexNumber!))!..<(encodedURL?.index((encodedURL?.endIndex)!, offsetBy: 0))!))
+                    
+                    let validURL = URL(string: validURLString!)
+                
+
+                    //safari起動
+                let safariVC = SFSafariViewController(url: validURL!)
+                self.present(safariVC, animated: true, completion: nil)
+                
+                    
+            } else if let httpRange = encodedURL?.range(of: "http") {
+                //Httpの場合
+                let startPosition = encodedURL?.characters.distance(from: (encodedURL?.characters.startIndex)!, to: httpRange.lowerBound)
+                
+                //4番目から最後までをURLとして扱う
+                
+                let indexNumber = startPosition
+                
+                let validURLString = (encodedURL?.substring(with: (encodedURL?.index((encodedURL?.startIndex)!, offsetBy: indexNumber!))!..<(encodedURL?.index((encodedURL?.endIndex)!, offsetBy: 0))!))
+                
+                let validURL = URL(string: validURLString!)
+                
+                //safari起動
+                let safariVC = SFSafariViewController(url: validURL!)
+                self.present(safariVC, animated: true, completion: nil)
+                
+                
+                
+                
+                
+                
+            }
         
+        else {
+                   }
         
+       
+        } else {
+            //そもそもhttp(s)がない場合
+            print("無効なURL")
+            //アラート表示
+            let alertController = UIAlertController(title: "エラー", message: "URLが無効なようです", preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "Ok", style: .default) {
+                (action) in
+                self.dismiss(animated: true, completion: nil)
+            }
+            
+            alertController.addAction(okAction)
+            self.present(alertController, animated: true, completion: nil)
+            
+            
+            
+            
+        }
         
         
         
