09eb719d6a82f6c1b417321b6a2bceb40d3a9cb4
Yujiro Saito <yujiro@Yujiro-Saito-no-MacBook-Air.local>
2017-08-27 16:47:14 +0900

    ボード改善

--- Project6/MyCollectionViewController.swift
+++ Project6/MyCollectionViewController.swift
@@ -8,56 +8,53 @@
 
 import UIKit
 
-
-class MyCollectionViewController: UICollectionViewController {
-
-    struct Storyboard {
-        
-        static let leftAndRightPaddings: CGFloat = 32.0 // 3 items per row, meaning 4 paddings of 8 each
-        static let numberOfItemsPerRow: CGFloat = 3.0
-        static let titleHeightAdjustment: CGFloat = 30.0
-    }
+class MyCollectionViewController: UIViewController,UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     
+    
+    @IBOutlet weak var myCollection: UICollectionView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let collectionViewWidth = collectionView?.frame.width
-        let itemWidth = (collectionViewWidth! - Storyboard.leftAndRightPaddings) / Storyboard.numberOfItemsPerRow
+        myCollection.delegate = self
+        myCollection.dataSource = self
         
-        let layout = collectionViewLayout as! UICollectionViewFlowLayout
-        layout.itemSize = CGSize(width: itemWidth, height: itemWidth + Storyboard.titleHeightAdjustment)
+
     }
     
     
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
     
-    
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 15
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "myCollection", for: indexPath) as? MyCollectionViewCell
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = myCollection.dequeueReusableCell(withReuseIdentifier: "myCollectionCell", for: indexPath) as? MyCollectionViewCell
         
-        cell?.itemImage.image = UIImage(named: "Sunset")
-        cell?.itemTitleLabel.text = "Port App That is amaizin"
-        // Configure the cell
         
         return cell!
     }
     
-    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView
-    {
-        let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "Head", for: indexPath) as! SectionHeaderCollectionReusableView
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let screenWidth = UIScreen.main.bounds.width
+        let scaleFactor = (screenWidth / 3) - 6
         
-        
-        return headerView
+        return CGSize(width: scaleFactor, height: scaleFactor + 45)
     }
     
+    //縦の間隔を決定する
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    //横の間隔
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 3
+    }
     
+
+ 
 }
